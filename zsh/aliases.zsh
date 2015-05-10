b4b9ad3f73bf2d909b7a5856f4e7327f7a397a73
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-05-10 16:09:23 -0600

    added aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,31 @@
-#	Github		#
+#
+#	Git 				
+#
 
 alias gs="git status"
+alias ga="git add"
+alias ga.="git add ."
+alias gc="git commit -m"
+
+#
+#	Shell Navigation			
+#
+
+alias df="cd ~/.dotfiles && subl ."
+alias notes="subl ~/Documents/notes.md"
+alias s="subl"
+alias s.="subl ."
+alias path="echo '$PATH' | tr : '\n'"   		# display path for troubleshooting
+
+#
+#	Homebrew
+#
+
+alias brewd="brew doctor"
+alias brewu="brew update && brew upgrade"
+
+#
+#	Applications			
+#
+
+alias chrome=
