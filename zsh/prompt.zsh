9b480f874f942a50e489ffc35f9620ce19b3959a
Lovisa <lovisa@Lovisas-MacBook-Pro.local>
2018-05-09 09:05:17 -0600

    Improve how branch is parsed
    thanks: https://coderwall.com/p/fasnya/add-git-branch-name-to-bash-prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,8 @@
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
 prompt () {
-    PROMPT='✨ %F{blue}$(pwd)%f%F{green}$(if [ -e .git ]; then git branch 2>/dev/null | grep '^*' | tr "*" ":"; fi)%f  '
+    PROMPT="✨ %F{blue}$(pwd)%f%F{yellow}$(parse_git_branch)%f "
     RPROMPT='%F{yellow}⏰  $(date +"%H:%M")%f'
 }
