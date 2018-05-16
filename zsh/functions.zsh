13e5265461878853db47f4ef921ddd982226e65f
Lovisa Svallingson <lovisa@flowhub.co>
2018-05-16 16:29:50 -0600

    Add function to remove console.logs

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -18,5 +18,18 @@ setalias() {
 # $1: the location you want to check
 wtw() {
   location=$(echo "$@" | tr " " "+")
-  curl wttr.in/$location?m
+  curl "wttr.in/$location?m"
+}
+
+# Removes console.log's in changed files tracked by git
+# $1: Optional. File to remove entries from. If no argument is given, it will modify all changed files.
+rmCL() {
+  if [ $# -ne 0 ]; then
+    sed '/console.log/d' $1 > tmpfile && mv tmpfile $1    
+  else 
+    for file in $(git diff --name-only); do
+      echo $file
+      sed '/console.log/d' $file > tmpfile && mv tmpfile $file
+    done
+  fi
 }
