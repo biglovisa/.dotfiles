91518b542a4288da34b182f8b39c1abdba48f78b
Lovisa Svallingson <lovisasvallingson@gmail.com>
2018-05-28 15:45:02 -0600

    Fix prompt: wasn't checking if git file exists

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,10 @@
 parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+    if [ -e .git ]; then 
+        git branch 2>/dev/null | grep '^*' | tr "*" ":"; 
+    fi
 }
 
 prompt () {
-    PROMPT="✨ %F{blue}$(pwd)%f%F{yellow}$(parse_git_branch)%f "
+    PROMPT="✨ %F{blue}$(pwd)%f%F{green}$(parse_git_branch)%f "
     RPROMPT='%F{yellow}⏰  $(date +"%H:%M")%f'
 }
